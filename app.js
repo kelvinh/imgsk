2ddb751b18a7637682230318cb688bd20900f761
Kelvin Hu <ini.kelvin@gmail.com>
2015-09-27 19:00:24 +0800

    add db initialization

--- app.js
+++ app.js
@@ -13,6 +13,10 @@ var http = require('http');
 var log = require('./lib/logger');
 var web = require('./routes/index');
 var api = require('./routes/api');
+var db = require('./lib/db');
+var userModel = require('./models/user');
+var imageModel = require('./models/image');
+var userImageModel = require('./models/user-image');
 
 passport.serializeUser(function(user, done) {
     log.debug('serializeUser: ', user);
@@ -66,6 +70,28 @@ if (config.debug) {
 var port = process.env.OPENSHIFT_NODEJS_PORT || config.port;
 var addr = process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 app.set('port', port);
+
+// under debug mode, we drop all tables and re-create them; under
+// production mode, we only test if connection can be established.
+//
+// TODO: currently the application can still run even if there is
+// error connecting database, the control flow should be chained,
+// the followed steps should only continue when there is no error.
+if (config.debug) {
+    log.debug('re-creating tables...');
+    db.sync({ force: true }).then(function() {
+        log.debug('tables re-created.');
+    }).catch(function(err) {
+        log.error('table re-creation error: ', err);
+    });
+} else {
+    log.debug('test db connection...');
+    db.authenticate().then(function() {
+        log.debug('db connected.');
+    }).catch(function(err) {
+        log.error('db connection error: ', err);
+    });
+}
 
 var server = http.createServer(app);
 server.listen(port, addr);
