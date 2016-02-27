ff8a4a0e11329b17035369350370702470764e76
Kelvin Hu <ini.kelvin@gmail.com>
2016-02-28 01:19:17 +0800

    add db initialization

--- mushroom.js
+++ mushroom.js
@@ -2,5 +2,33 @@
 
 var config = require('./config');
 var server = require('./lib/server');
+var db = require('./lib/db');
+var log = require('./lib/logger');
+var User = require('./models/user');
+var Image = require('./models/image');
+var UserImage = require('./models/user-image');
+
+if (config.debug) {
+    log.info('recreating tables...');
+    db.sync({
+        force: true
+    }).then(function() {
+        log.info('tables recreated.');
+        return User.create({
+            name: 'kelvin',
+            email: 'i@a.com',
+            password: '123'
+        });
+    }).catch(function(err) {
+        log.error('table recreation error: ', err);
+    });
+} else {
+    log.info('test db connection...');
+    db.authenticate().then(function() {
+        log.info('db connected.');
+    }).catch(function(err) {
+        log.error('db connection error: ', err);
+    });
+}
 
 server.start(config);
